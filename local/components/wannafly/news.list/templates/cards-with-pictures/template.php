af3f05eab1284a5462a3fe7969dd1059fa5a7a36
Julia Bragina <triulya@yandex.ru>
2024-10-22 18:53:33 +0400

    не использкем тег a если нет ссылки cards-with-pictures

--- local/components/wannafly/news.list/templates/cards-with-pictures/template.php
+++ local/components/wannafly/news.list/templates/cards-with-pictures/template.php
@@ -2,7 +2,6 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
-// ... другие переменные
 $this->setFrameMode(true);
 ?>
 
@@ -26,33 +25,42 @@ $this->setFrameMode(true);
     <div class="readyProjects__container readyProjects__sliderContainer slider swiper-container">
         <ul class="slider__items swiper-wrapper">
             <?php foreach ($arResult["ITEMS"] as $item): ?>
-                <a class="slider__link swiper-slide" href="#">
-                    <li class="slider__item slider__item_hover" id="<?= $this->GetEditAreaId($item['ID']); ?>">
-                        <?php
-                        if (!empty($item["PREVIEW_PICTURE"])) {
+                <?php
+                    $link = !empty($item["PROPERTIES"]["LINK"]["VALUE"]) ? $item["PROPERTIES"]["LINK"]["VALUE"] : null; // Предположим, что ссылка хранится в свойствах "LINK"
+                ?>
 
-                            $resizedImage = CFile::ResizeImageGet(
-                                $item["PREVIEW_PICTURE"]['ID'],
-                                array("width" => 295, "height" => 350),
-                                BX_RESIZE_IMAGE_EXACT
-                            );
+                <?php if ($link):?>
+                    <a class="slider__link swiper-slide" href="<?= $link ?>">
+                <?php else: ?>
+                    <div class="slider__link swiper-slide">
+                <?php endif; ?>
+                        <li class="slider__item slider__item_hover" id="<?= $this->GetEditAreaId($item['ID']); ?>">
+                            <?php if (!empty($item["PREVIEW_PICTURE"])) {
+                                $resizedImage = CFile::ResizeImageGet(
+                                    $item["PREVIEW_PICTURE"]['ID'],
+                                    array("width" => 295, "height" => 350),
+                                    BX_RESIZE_IMAGE_EXACT
+                                );
                             ?>
-                            <img class="slider__img" src="<?= $resizedImage['src'] ?>" alt="<?= $item['NAME'] ?>">
-                        <?php } ?>
-                        <div class="readyProjects__text">
-                            <?php if ($arParams['TAGS_STYLE'] == 'style2'): ?>
-                                <p class="slider__descriptioin readyProjects__description readyProjects__description_color_pink"><?php echo $item["PROPERTIES"]["TAGS"]["VALUE"]["TEXT"]; ?>
-                                </p>
-                            <?php endif; ?>
-                            <h3 class="slider__title readyProjects__title title__third">
-                                <?php echo $item["NAME"]; ?>
-                            </h3>
-                            <?php if ($arParams['TAGS_STYLE'] == 'style1'): ?>
-                                <p class="slider__descriptioin readyProjects__description"><?php echo $item["PROPERTIES"]["TAGS"]["VALUE"]["TEXT"]; ?></p>
-                            <?php endif; ?>
-                        </div>
-                    </li>
-                </a>
+                                <img class="slider__img" src="<?= $resizedImage['src'] ?>" alt="<?= $item['NAME'] ?>">
+                            <?php } ?>
+                            <div class="readyProjects__text">
+                                <?php if ($arParams['TAGS_STYLE'] == 'style2'): ?>
+                                    <p class="slider__descriptioin readyProjects__description readyProjects__description_color_pink"><?php echo $item["PROPERTIES"]["TAGS"]["VALUE"]["TEXT"]; ?></p>
+                                <?php endif; ?>
+                                <h3 class="slider__title readyProjects__title title__third">
+                                    <?php echo $item["NAME"]; ?>
+                                </h3>
+                                <?php if ($arParams['TAGS_STYLE'] == 'style1'): ?>
+                                    <p class="slider__descriptioin readyProjects__description"><?php echo $item["PROPERTIES"]["TAGS"]["VALUE"]["TEXT"]; ?></p>
+                                <?php endif; ?>
+                            </div>
+                        </li>
+                <?php if ($link): ?>
+                    </a>
+                <?php else: ?>
+                </div>
+                <?php endif; ?>
             <?php endforeach; ?>
         </ul>
     </div>
