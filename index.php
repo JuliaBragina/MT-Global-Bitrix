d7af110d1be9a355305d0c36e9bb53a36216f05a
Julia Bragina <triulya@yandex.ru>
2024-10-28 16:07:11 +0400

    тесторивание отправки

--- index.php
+++ index.php
@@ -463,24 +463,6 @@ $APPLICATION->SetTitle("Главная");
 		"TAGS_STYLE" => "style2",
 		"TITLE" => "База знаний MT GLOBAL"
 	)
-);?><?$APPLICATION->IncludeComponent("bitrix:form.result.new", "callback", Array(
-	"CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHAIN_ITEM_LINK" => "",	// Ссылка на дополнительном пункте в навигационной цепочке
-		"CHAIN_ITEM_TEXT" => "",	// Название дополнительного пункта в навигационной цепочке
-		"EDIT_URL" => "result_edit.php",	// Страница редактирования результата
-		"IGNORE_CUSTOM_TEMPLATE" => "N",	// Игнорировать свой шаблон
-		"LIST_URL" => "result_list.php",	// Страница со списком результатов
-		"SEF_MODE" => "N",	// Включить поддержку ЧПУ
-		"SUCCESS_URL" => "",	// Страница с сообщением об успешной отправке
-		"USE_EXTENDED_ERRORS" => "N",	// Использовать расширенный вывод сообщений об ошибках
-		"VARIABLE_ALIASES" => array(
-			"RESULT_ID" => "RESULT_ID",
-			"WEB_FORM_ID" => "WEB_FORM_ID",
-		),
-		"WEB_FORM_ID" => "4",	// ID веб-формы
-	),
-	false
 );?></main><main></main><?php
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
 ?>
