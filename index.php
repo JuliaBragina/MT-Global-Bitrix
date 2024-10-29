23de49e3506fb578d84eaff597ca2433acbd30f6
Julia Bragina <triulya@yandex.ru>
2024-10-29 14:22:55 +0400

    изменена логика статических форм (чтобы не пропадали при успешной отправке данных)

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ $APPLICATION->SetTitle("Главная");
 		"STRICT_SECTION_CHECK" => "N",
 		"TITLE" => "Услуги"
 	)
-);?></main><main class="content"></main><main></main><main class="content"></main> <main class="content"> <?$APPLICATION->IncludeComponent(
+);?><?$APPLICATION->IncludeComponent(
 	"wannafly:news.list",
 	"grid-cards",
 	Array(
@@ -188,7 +188,7 @@ $APPLICATION->SetTitle("Главная");
 		"TAGS_STYLE" => "style1",
 		"TITLE" => "О компании"
 	)
-);?></main><main><?$APPLICATION->IncludeComponent(
+);?><?$APPLICATION->IncludeComponent(
 	"wannafly:compass",
 	"",
 	Array(
@@ -334,7 +334,7 @@ $APPLICATION->SetTitle("Главная");
 		"TAGS_STYLE" => "style1",
 		"TITLE" => "Реализованные проекты"
 	)
-);?></main><main><?$APPLICATION->IncludeComponent(
+);?><?$APPLICATION->IncludeComponent(
 	"wannafly:news.list",
 	"reviews",
 	Array(
@@ -473,68 +473,7 @@ $APPLICATION->SetTitle("Главная");
 		"TAGS_STYLE" => "style2",
 		"TITLE" => "База знаний MT GLOBAL"
 	)
-);?></main><main><?$APPLICATION->IncludeComponent(
-	"wannafly:news.list",
-	"cards-with-pictures",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "cards-with-pictures",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(0=>"",1=>"",),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "71",
-		"IBLOCK_TYPE" => "main",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "20",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(0=>"TAGS",1=>"",),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"TAGS_STYLE" => "style2",
-		"TITLE" => "База знаний MT GLOBAL"
-	)
-);?><br>
- <br>
- <br>
+);?>
  <br>
  <?$APPLICATION->IncludeComponent(
 	"bitrix:form.result.new",
@@ -559,6 +498,6 @@ $APPLICATION->SetTitle("Главная");
 		"AJAX_OPTION_HISTORY" => "N",
 	)
 );?><br>
- </main><main></main><?php
+ </main><?php
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
 ?>
--- local/templates/wannafly/components/bitrix/form.result.new/briefing/template.php
+++ local/templates/wannafly/components/bitrix/form.result.new/briefing/template.php
@@ -1,98 +1,97 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?php if ($arResult["isFormNote"] != "Y"): ?>
-    <section class="contactForm">
-        <div class="contactForm__container">
-            <div class="contactForm__instructions">
-                <h2 class="contactForm__title title__second title__second_hight">Расскажите нам о вашей задаче</h2>
-                <ol class="contactForm__steps">
-                    <li class="contactForm__step">Отправьте нам заполненную форму и прикрепите файлы для
-                        ознакомления, если возможно. Это поможет нам быстрее подобрать оптимальное решение.
-                    </li>
-                    <li class="contactForm__step">После отправки заявки наш специалист ознакомится с информацией и
-                        свяжется с вами.
-                    </li>
-                </ol>
-                <p class="contactForm__note">Позвоните или напишите нам на электронный адрес. Так мы сможем
-                    оперативно подобрать решение, которое подойдет вам лучше всего.</p>
-                <div class="contactForm__contacts">
-                    <p class="contactForm__contactsTitle">Контакты</p>
-                    <div class="wrapper d-flex">
-                        <?php $APPLICATION->IncludeFile(
-                            SITE_DIR . 'includes/phone_getSupport.php',
-                            array(),
-                            array("MODE" => 'html')
-                        ); ?>
-                        <?php $APPLICATION->IncludeFile(
-                            SITE_DIR . 'includes/email.php',
-                            array(),
-                            array("MODE" => 'html')
-                        ); ?>
-                    </div>
+<section class="contactForm" style="<?= $arResult["isFormNote"] == "Y" ? 'opacity: 1; top: 0;' : '' ?>">
+    <div class="contactForm__container">
+        <div class="contactForm__instructions">
+            <h2 class="contactForm__title title__second title__second_hight">Расскажите нам о вашей задаче</h2>
+            <ol class="contactForm__steps">
+                <li class="contactForm__step">Отправьте нам заполненную форму и прикрепите файлы для
+                    ознакомления, если возможно. Это поможет нам быстрее подобрать оптимальное решение.
+                </li>
+                <li class="contactForm__step">После отправки заявки наш специалист ознакомится с информацией и
+                    свяжется с вами.
+                </li>
+            </ol>
+            <p class="contactForm__note">Позвоните или напишите нам на электронный адрес. Так мы сможем
+                оперативно подобрать решение, которое подойдет вам лучше всего.</p>
+            <div class="contactForm__contacts">
+                <p class="contactForm__contactsTitle">Контакты</p>
+                <div class="wrapper d-flex">
+                    <?php $APPLICATION->IncludeFile(
+                        SITE_DIR . 'includes/phone_getSupport.php',
+                        array(),
+                        array("MODE" => 'html')
+                    ); ?>
+                    <?php $APPLICATION->IncludeFile(
+                        SITE_DIR . 'includes/email.php',
+                        array(),
+                        array("MODE" => 'html')
+                    ); ?>
                 </div>
             </div>
-
-            <form class="contactForm__submission" action="<?= POST_FORM_ACTION_URI ?>" method="POST"
-                enctype="multipart/form-data" <?=$this->GetEditAreaId($arResult['ID']);?>>
-                <?= bitrix_sessid_post(); ?>
-                <?= $arResult["FORM_HEADER"] ?>
-
-                <h3 class="contactForm__formTitle title__third title__third_color_white">Заполните форму</h3>
-
-                <!-- Поля формы -->
-                <fieldset class="contactForm__inputFieldset">
-                    <div class="contactForm__inputWrapper contactForm__nameWrapper">
-                        <?php
-                            $nameHTML = $arResult["QUESTIONS"]["name"]["HTML_CODE"];
-                            $nameHTML = str_replace('>', 'id="popup__name" required>', $nameHTML);
-                            echo $nameHTML;
-                        ?>
-                        <label for="name"
-                            class="contactForm__label"><?= $arResult["QUESTIONS"]["name"]["CAPTION"] ?></label>
-                    </div>
-                    <div class="contactForm__inputWrapper popup__field_phone">
-                        <?php
-                            $phoneHTML = $arResult["QUESTIONS"]["phone"]["HTML_CODE"];
-                            $phoneHTML = str_replace('>', 'required id="popup__phone">', $phoneHTML);
-                            echo $phoneHTML;
-                        ?>
-                        <label for="phone"
-                            class="contactForm__label"><?= $arResult["QUESTIONS"]["phone"]["CAPTION"] ?></label>
-                    </div>
-                </fieldset>
-
-                <fieldset class="contactForm__inputFieldset">
-                    <div class="contactForm__inputWrapper">
-                        <?php
-                            $emailHTML = $arResult["QUESTIONS"]["email"]["HTML_CODE"];
-                            $emailHTML = str_replace('>', 'required id="popup__email">', $emailHTML);
-                            echo $emailHTML;
-                        ?>
-                        <label for="email"
-                            class="contactForm__label"><?= $arResult["QUESTIONS"]["email"]["CAPTION"] ?></label>
-                    </div>
-                </fieldset>
-
-                <fieldset class="contactForm__inputFieldset">
-                    <div class="contactForm__inputWrapper">
-                        <?= $arResult["QUESTIONS"]["projectDescription"]["HTML_CODE"] ?>
-                        <label for="projectDescription"
-                            class="contactForm__label"><?= $arResult["QUESTIONS"]["projectDescription"]["CAPTION"] ?></label>
-                    </div>
-                </fieldset>
-
-                <input type="submit" class="btn btn-primary contactForm__submit header__callback-button" id="popup__btn-submit" name="web_form_submit" value="<?= htmlspecialcharsbx($arResult['arForm']['BUTTON']) ?>">
-
-                <label class="contactForm__checkboxContainer">
-                    <?= $arResult["QUESTIONS"]["agreement"]["HTML_CODE"] ?>
-                    <p class="contactForm__textCheckbox"><?= $arResult["QUESTIONS"]["agreement"]["CAPTION"] ?></p>
-                </label>
-
-                <?= $arResult["FORM_FOOTER"] ?>
-            </form>
         </div>
-    </section>
-<?php else: ?>
+
+        <form class="contactForm__submission" action="<?= POST_FORM_ACTION_URI ?>" method="POST"
+            enctype="multipart/form-data" <?=$this->GetEditAreaId($arResult['ID']);?>>
+            <?= bitrix_sessid_post(); ?>
+            <?= $arResult["FORM_HEADER"] ?>
+
+            <h3 class="contactForm__formTitle title__third title__third_color_white">Заполните форму</h3>
+
+            <!-- Поля формы -->
+            <fieldset class="contactForm__inputFieldset">
+                <div class="contactForm__inputWrapper contactForm__nameWrapper">
+                    <?php
+                        $nameHTML = $arResult["QUESTIONS"]["name"]["HTML_CODE"];
+                        $nameHTML = str_replace('>', 'id="popup__name" required>', $nameHTML);
+                        echo $nameHTML;
+                    ?>
+                    <label for="name"
+                        class="contactForm__label"><?= $arResult["QUESTIONS"]["name"]["CAPTION"] ?></label>
+                </div>
+                <div class="contactForm__inputWrapper popup__field_phone">
+                    <?php
+                        $phoneHTML = $arResult["QUESTIONS"]["phone"]["HTML_CODE"];
+                        $phoneHTML = str_replace('>', 'required id="popup__phone">', $phoneHTML);
+                        echo $phoneHTML;
+                    ?>
+                    <label for="phone"
+                        class="contactForm__label"><?= $arResult["QUESTIONS"]["phone"]["CAPTION"] ?></label>
+                </div>
+            </fieldset>
+
+            <fieldset class="contactForm__inputFieldset">
+                <div class="contactForm__inputWrapper">
+                    <?php
+                        $emailHTML = $arResult["QUESTIONS"]["email"]["HTML_CODE"];
+                        $emailHTML = str_replace('>', 'required id="popup__email">', $emailHTML);
+                        echo $emailHTML;
+                    ?>
+                    <label for="email"
+                        class="contactForm__label"><?= $arResult["QUESTIONS"]["email"]["CAPTION"] ?></label>
+                </div>
+            </fieldset>
+
+            <fieldset class="contactForm__inputFieldset">
+                <div class="contactForm__inputWrapper">
+                    <?= $arResult["QUESTIONS"]["projectDescription"]["HTML_CODE"] ?>
+                    <label for="projectDescription"
+                        class="contactForm__label"><?= $arResult["QUESTIONS"]["projectDescription"]["CAPTION"] ?></label>
+                </div>
+            </fieldset>
+
+            <input type="submit" class="btn btn-primary contactForm__submit header__callback-button" id="popup__btn-submit" name="web_form_submit" value="<?= htmlspecialcharsbx($arResult['arForm']['BUTTON']) ?>">
+
+            <label class="contactForm__checkboxContainer">
+                <?= $arResult["QUESTIONS"]["agreement"]["HTML_CODE"] ?>
+                <p class="contactForm__textCheckbox"><?= $arResult["QUESTIONS"]["agreement"]["CAPTION"] ?></p>
+            </label>
+
+            <?= $arResult["FORM_FOOTER"] ?>
+        </form>
+    </div>
+</section>
+<?php if ($arResult["isFormNote"] == "Y"): ?>
     <script>
         Fancybox.show([{
             src: "#thanks2",
@@ -103,4 +102,3 @@
         }, 5000);
     </script>
 <?php endif; ?>
-
