e5b8a742decd2bc2d1e6db5b0c0a46dafc706e46
Julia Bragina <triulya@yandex.ru>
2024-11-05 10:01:13 +0400

    инпут формы сделан поярче

--- local/templates/wannafly/components/bitrix/form.result.new/presentation/template.php
+++ local/templates/wannafly/components/bitrix/form.result.new/presentation/template.php
@@ -1,55 +1,56 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<div class="getPresentationSection__container">
+<section class="getPresentationSection">
+    <div class="getPresentationSection__container">
 
-    <form class="getPresentationForm" action="<?= POST_FORM_ACTION_URI ?>" method="POST"
-            id="<?= $this->GetEditAreaId($arResult['ID']); ?>">
-        <?= bitrix_sessid_post(); ?>
+        <form class="getPresentationForm" action="<?= POST_FORM_ACTION_URI ?>" method="POST"
+                id="<?= $this->GetEditAreaId($arResult['ID']); ?>">
+            <?= bitrix_sessid_post(); ?>
 
-        <?= $arResult["FORM_HEADER"] ?>
-        <h2 class="getPresentationSection__title title__second">
-            Презентация о MT global
-        </h2>
+            <?= $arResult["FORM_HEADER"] ?>
+            <h2 class="getPresentationSection__title title__second">
+                Презентация о MT global
+            </h2>
 
-        <!-- Поле "Имя" -->
-        <div class="getPresentationForm__field">
-            <label for="name" class="getPresentationForm__label">
-                <?= $arResult["QUESTIONS"]["name"]["CAPTION"] ?>
-                <?php if ($arResult["QUESTIONS"]["name"]["REQUIRED"] == "Y"): ?>
-                    <span class="required"><?= $arResult["REQUIRED_SIGN"]; ?></span>
-                <?php endif; ?>
-            </label>
-            <?php
-                $nameHTML = $arResult["QUESTIONS"]["name"]["HTML_CODE"];
-                $nameHTML = str_replace('>', 'id="popup__name" required>', $nameHTML);
-                echo $nameHTML;
-            ?>
-            <span class="popup__error"></span>
-        </div>
+            <!-- Поле "Имя" -->
+            <div class="getPresentationForm__field">
+                <label for="name" class="getPresentationForm__label">
+                    <?= $arResult["QUESTIONS"]["name"]["CAPTION"] ?>
+                    <?php if ($arResult["QUESTIONS"]["name"]["REQUIRED"] == "Y"): ?>
+                        <span class="required"><?= $arResult["REQUIRED_SIGN"]; ?></span>
+                    <?php endif; ?>
+                </label>
+                <?php
+                    $nameHTML = $arResult["QUESTIONS"]["name"]["HTML_CODE"];
+                    $nameHTML = str_replace('>', 'id="popup__name" required>', $nameHTML);
+                    echo $nameHTML;
+                ?>
+                <span class="popup__error"></span>
+            </div>
 
-        <!-- Поле "Почта" -->
-        <div class="getPresentationForm__field">
-            <label for="email" class="getPresentationForm__label">
-                <?= $arResult["QUESTIONS"]["email"]["CAPTION"] ?>
-                <?php if ($arResult["QUESTIONS"]["email"]["REQUIRED"] == "Y"): ?>
-                    <span class="required"><?= $arResult["REQUIRED_SIGN"]; ?></span>
-                <?php endif; ?>
-            </label>
-            <?php
-                $emailHTML = $arResult["QUESTIONS"]["email"]["HTML_CODE"];
-                $emailHTML = str_replace('>', 'required id="popup__email">', $emailHTML);
-                echo $emailHTML;
-            ?>
-            <span class="popup__error"></span>
-        </div>
+            <!-- Поле "Почта" -->
+            <div class="getPresentationForm__field">
+                <label for="email" class="getPresentationForm__label">
+                    <?= $arResult["QUESTIONS"]["email"]["CAPTION"] ?>
+                    <?php if ($arResult["QUESTIONS"]["email"]["REQUIRED"] == "Y"): ?>
+                        <span class="required"><?= $arResult["REQUIRED_SIGN"]; ?></span>
+                    <?php endif; ?>
+                </label>
+                <?php
+                    $emailHTML = $arResult["QUESTIONS"]["email"]["HTML_CODE"];
+                    $emailHTML = str_replace('>', 'required id="popup__email">', $emailHTML);
+                    echo $emailHTML;
+                ?>
+                <span class="popup__error"></span>
+            </div>
 
-        <!-- Кнопка отправки формы -->
-        <input type="submit" class="btn btn-secondary-grey getPresentationForm__button popup__button" id="popup__btn-submit" name="web_form_submit" value="<?= htmlspecialcharsbx($arResult['arForm']['BUTTON']) ?>">
+            <!-- Кнопка отправки формы -->
+            <input type="submit" class="btn btn-secondary-grey getPresentationForm__button popup__button" id="popup__btn-submit" name="web_form_submit" value="<?= htmlspecialcharsbx($arResult['arForm']['BUTTON']) ?>">
 
-        <?= $arResult["FORM_FOOTER"] ?>
-    </form>
+            <?= $arResult["FORM_FOOTER"] ?>
+        </form>
 
-    <!-- Изображение с презентацией -->
-    <img class="getPresentationSection__img" src="<?= SITE_TEMPLATE_PATH ?>/img/img-pdf.svg" alt="Презентация">
-</div>
+        <!-- Изображение с презентацией -->
+        <img class="getPresentationSection__img" src="<?= SITE_TEMPLATE_PATH ?>/img/img-pdf.svg" alt="Презентация">
+    </div>
 </section>
