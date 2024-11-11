fcb69f2f0d48e6b545793e673d8d539a1abe294d
Юлия Брагина <juliabragina@MacBookPro.home>
2024-11-11 11:02:43 +0400

    поправл квиз

--- includes/quiz.php
+++ includes/quiz.php
@@ -1,24 +1,24 @@
-&lt;!-- Marquiz script start --&gt;<br>
- &lt;script&gt;<br>
- (function(w, d, s, o){<br>
- &nbsp; var j = d.createElement(s); j.async = true; j.src = '//script.marquiz.ru/v2.js';j.onload = function() {<br>
- &nbsp; &nbsp; if (document.readyState !== 'loading') Marquiz.init(o);<br>
- &nbsp; &nbsp; else document.addEventListener("DOMContentLoaded", function() {<br>
- &nbsp; &nbsp; &nbsp; Marquiz.init(o);<br>
- &nbsp; &nbsp; });<br>
- &nbsp; };<br>
- &nbsp; d.head.insertBefore(j, d.head.firstElementChild);<br>
- })(window, document, 'script', {<br>
- &nbsp; &nbsp; host: '//quiz.marquiz.ru',<br>
- &nbsp; &nbsp; region: 'ru',<br>
- &nbsp; &nbsp; id: '672d92f0ef41d200262e264b',<br>
- &nbsp; &nbsp; autoOpen: false,<br>
- &nbsp; &nbsp; autoOpenFreq: 'once',<br>
- &nbsp; &nbsp; openOnExit: false,<br>
- &nbsp; &nbsp; disableOnMobile: false<br>
- &nbsp; }<br>
- );<br>
- &lt;/script&gt;<br>
- &lt;!-- Marquiz script end --&gt;<br>
- &lt;div data-marquiz-id="672d92f0ef41d200262e264b"&gt;&lt;/div&gt;<br>
- &lt;script&gt;(function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Inline', {id: '672d92f0ef41d200262e264b', buttonText: '«Старт»', bgColor: '#ed7180', textColor: '#ffffff', rounded: true, shadow: 'rgba(237, 113, 128, 0.5)', blicked: true, buttonOnMobile: true, height: '520'})&lt;/script&gt;<br>
+<!-- Marquiz script start -->
+<script>
+(function(w, d, s, o){
+  var j = d.createElement(s); j.async = true; j.src = '//script.marquiz.ru/v2.js';j.onload = function() {
+    if (document.readyState !== 'loading') Marquiz.init(o);
+    else document.addEventListener("DOMContentLoaded", function() {
+      Marquiz.init(o);
+    });
+  };
+  d.head.insertBefore(j, d.head.firstElementChild);
+})(window, document, 'script', {
+    host: '//quiz.marquiz.ru',
+    region: 'ru',
+    id: '672d92f0ef41d200262e264b',
+    autoOpen: false,
+    autoOpenFreq: 'once',
+    openOnExit: false,
+    disableOnMobile: false
+  }
+);
+</script>
+<!-- Marquiz script end -->
+<div data-marquiz-id="672d92f0ef41d200262e264b"></div>
+<script>(function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Inline', {id: '672d92f0ef41d200262e264b', buttonText: '«Старт»', bgColor: '#ed7180', textColor: '#ffffff', rounded: true, shadow: 'rgba(237, 113, 128, 0.5)', blicked: true, buttonOnMobile: true, height: '520'})</script>
