16d45d68497d2851828993543fdc493544f66abe
Юлия Брагина <juliabragina@MacBookPro.home>
2024-11-11 10:14:20 +0400

    добавлена плавность показа меню и убрана тень

--- local/templates/wannafly/js/main.js
+++ local/templates/wannafly/js/main.js
@@ -28,15 +28,17 @@ window.onscroll = function() {
     let currentScrollY = window.pageYOffset;
 
     if (currentScrollY > lastScrollY) {
-        menu.style.transform = "translateY(-1000%) translateX(-50%)";
+        menu.style.transform = "translateY(-1000%)";
         header.style.boxShadow = '0px 4px 10px rgba(34, 60, 80, 0.25)';
         menu.style.opacity = 0;
     } else {
-        menu.style.transform = "translateY(0) translateX(-50%)";
+        menu.style.transform = "translateY(0)";
         menu.style.position = "fixed";
         menu.style.top = "80.2px";
         menu.style.left = "50%";
+        menu.style.transform = "translateX(-50%)";
         menu.style.zIndex = "1000";
+        menu.style.boxShadow = '0px 4px 10px rgba(34, 60, 80, 0.25)';
         menu.style.maxWidth = "1920px";
         menu.style.width = "100%";
         menu.style.opacity = 1;
