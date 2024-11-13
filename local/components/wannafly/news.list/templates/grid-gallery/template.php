e3725a0101765175be751ac747e04289d4bc32ca
Юлия Брагина <juliabragina@MacBookPro.home>
2024-11-13 09:20:42 +0400

    переделано на css анимацию

--- local/components/wannafly/news.list/templates/grid-gallery/template.php
+++ local/components/wannafly/news.list/templates/grid-gallery/template.php
@@ -1,11 +1,11 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
 
-<section id="certificates" class="certificates swiper">
+<section id="certificates" class="certificates">
     <div class="certificates__container container">
         <h2 class="certificates__title title__second title__bottom-margin"><?= $arParams['TITLE'] ?></h2>
-        <div class="certificates__wrap swiper-container">
-            <div class="certificates__flex running-line-container certificates__marquee swiper-wrapper">
+        <div class="certificates__wrap">
+            <div class="certificates__flex running-line-container certificates__marquee">
                 <?php if (!empty($arResult['ITEMS'])): ?>
                     <?php foreach ($arResult['ITEMS'] as $item): ?>
                         <?php if (!empty($item['PROPERTIES']['SERTIFICATES']['VALUE'])): ?>
@@ -17,7 +17,7 @@
                                 ], BX_RESIZE_IMAGE_PROPORTIONAL);
                                 ?>
                                 <?php if (!empty($certificateImage['src'])): ?>
-                                    <img class="certificates__img running-line-container__item swiper-slide" src="<?= $certificateImage['src']; ?>"
+                                    <img class="certificates__img running-line-container__item" src="<?= $certificateImage['src']; ?>"
                                             alt="Сертификат" id="<?= $this->GetEditAreaId($item['ID']); ?>">
                                 <?php endif; ?>
                             <?php endforeach; ?>
@@ -25,7 +25,6 @@
                     <?php endforeach; ?>
                 <?php endif; ?>
             </div>
-            <?php /*
             <div aria-hidden="true" class="certificates__flex running-line-container certificates__marquee">
                 <?php if (!empty($arResult['ITEMS'])): ?>
                     <?php foreach ($arResult['ITEMS'] as $item): ?>
@@ -45,7 +44,7 @@
                         <?php endif; ?>
                     <?php endforeach; ?>
                 <?php endif;?>
-            </div> */ ?>
+            </div>
         </div>
     </div>
 </section>
