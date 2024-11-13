19c986ce4e0ad20ecae1c2bf154d3916da3b6d3c
Юлия Брагина <juliabragina@MacBookPro.home>
2024-11-13 09:51:24 +0400

    убраны лишние стили

--- includes/phone_getConsultation.php
+++ includes/phone_getConsultation.php
@@ -1,2 +1,4 @@
-<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?> <a href="tel:88003334000" class="header__phone">
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+ <a href="tel:88003334000" class="header__phone">
 8 800 333-40-00 </a>
