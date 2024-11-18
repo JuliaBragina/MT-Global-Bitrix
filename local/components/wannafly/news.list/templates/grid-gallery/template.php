5369bb257574e5f1a1ea57388620933225d56ac9
Юлия Брагина <juliabragina@MacBookPro.home>
2024-11-18 19:18:20 +0400

    разделены галереи

--- local/components/wannafly/news.list/templates/grid-gallery/template.php
+++ local/components/wannafly/news.list/templates/grid-gallery/template.php
@@ -14,7 +14,7 @@
                                 ], BX_RESIZE_IMAGE_PROPORTIONAL);
                                 ?>
                                 <?php if (!empty($certificateImage['src'])): ?>
-                                    <a href="<?= CFile::GetPath($certificateId); ?>" class="certificates__link swiper-slide" data-fancybox="gallery" data-caption="Сертификат">
+                                    <a href="<?= CFile::GetPath($certificateId); ?>" class="certificates__link swiper-slide" data-fancybox="gallery1" data-caption="Сертификат">
                                         <img class="certificates__img running-line-container__item" src="<?= $certificateImage['src']; ?>"
                                             alt="Сертификат" id="<?= $this->GetEditAreaId($item['ID']); ?>">
                                     </a>
@@ -37,7 +37,7 @@
                                 ], BX_RESIZE_IMAGE_PROPORTIONAL);
                                 ?>
                                 <?php if (!empty($certificateImage['src'])): ?>
-                                    <a href="<?= CFile::GetPath($certificateId); ?>" class="certificates__link swiper-slide" data-fancybox="gallery" data-caption="Сертификат">
+                                    <a href="<?= CFile::GetPath($certificateId); ?>" class="certificates__link swiper-slide" data-fancybox="gallery2" data-caption="Сертификат">
                                         <img class="certificates__img running-line-container__item" src="<?= $certificateImage['src']; ?>"
                                             alt="Сертификат" id="<?= $this->GetEditAreaId($item['ID']); ?>">
                                     </a>
