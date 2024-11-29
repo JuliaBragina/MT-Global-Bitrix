f876ec4762a20606a5b162d5e20032d0545c9963
ftp <mtglobal_admin@thomas.beget.ru>
2024-11-29 13:12:47 +0300

    добавлены пункты меню

--- local/components/wannafly/news.list/templates/grid-cards-big/template.php
+++ local/components/wannafly/news.list/templates/grid-cards-big/template.php
@@ -7,7 +7,7 @@ $this->setFrameMode(true);
 
 <section class="ourSolutions section-container">
     <div class="ourSolutions__container container">
-        <h2 class="ourSolutions__title title__second title__bottom-margin"><?= $arParams['TITLE'] ?>й</h2>
+        <h2 class="ourSolutions__title title__second title__bottom-margin"><?= $arParams['TITLE'] ?></h2>
         <div class="ourSolutions__grid">
             <?php foreach ($arResult['ITEMS'] as $item):
                 $resizedImage = CFile::ResizeImageGet(
--- services/multimedia/.left.menu.php
+++ services/multimedia/.left.menu.php
@@ -6,6 +6,27 @@ $aMenuLinks = array(
         array(),
         array(),
         ""
+    ),
+    array(
+        "Ситуационные центры",
+        "/services/multimedia/situation-centers/",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "DIGITAL SIGNAGE",
+        "/services/multimedia/digital-signage/",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Конференц-залы",
+        "/services/multimedia/conference-halls/",
+        array(),
+        array(),
+        ""
     )
     // другие пункты третьего уровня
 );
