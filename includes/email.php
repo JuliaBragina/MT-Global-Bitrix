f7a73a8ff49d622c01daf1430876f5382fbc870a
ftp <mtglobal_admin@thomas.beget.ru>
2024-12-13 06:40:59 +0300

    поправлена почта

--- includes/email.php
+++ includes/email.php
@@ -1,5 +1,6 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<a href="tel:88003334000" class="contactForm__phone">
+
+<a href="mailto:info@mtglobal.ru" class="contactForm__phone">
     info@mtglobal.ru
 </a>
